2a0edbc33aafc58766c860947234f4c9107fe2c4
Martin Lédl <martin@code8.cz>
2024-02-24 10:28:35 +0100

    Initial and completion commit

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use clap::Parser;
+use colored::Colorize;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Parser)]
+pub struct Args {
+    /// The pattern to search for
+    pub pattern: String,
+    /// The file to search in
+    pub path: std::path::PathBuf,
+    /// Depth to search
+    #[clap(short, long, default_value = "1")]
+    pub depth: usize,
+    /// Output statistics
+    #[clap(short, long, default_value = "false")]
+    pub stats: bool,
+}
+
+pub fn find_matches(args: &Args, reader: BufReader<File>, buf: &mut String) {
+    for line_result in reader.lines() {
+        match line_result {
+            Ok(line) => {
+                if line.contains(args.pattern.as_str()) {
+                    let line = line.replace(
+                        args.pattern.as_str(),
+                        args.pattern.bright_red().bold().to_string().as_str(),
+                    );
+                    buf.push_str(&line);
+                    buf.push_str("\n");
+                }
+            }
+            // Err(e) => {
+            //     eprintln!("Error reading file: {}", e);
+            //     return;
+            // }
+            Err(_) => return,
+        }
+    }
+}
+
+pub fn process_file(
+    args: &Args,
+    path: &std::path::Path,
+    buf: &mut String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let mut current_file_buf = String::new();
+    find_matches(args, reader, &mut current_file_buf);
+    if current_file_buf.len() > 0 {
+        buf.push_str(&format!("\n\n{}:\n", path.display().to_string().green()));
+        buf.push_str(&current_file_buf);
+    }
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use anyhow::Result;
+use clap::Parser;
+use indicatif::ProgressBar;
+use rust_search::SearchBuilder;
+use std::io;
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = grape::Args::parse();
+    let files: Vec<String> = SearchBuilder::default()
+        .location(&args.path)
+        .depth(args.depth)
+        .build()
+        .collect();
+
+    let files_only = files.iter().filter(|path| {
+        let path = std::path::Path::new(path);
+        path.is_file()
+    });
+
+    let file_count = files_only.clone().count();
+    let directory_count = files.len() - file_count + 1;
+
+    let pb = ProgressBar::new(file_count as u64);
+
+    let mut buf = String::new();
+
+    for path in files_only {
+        let path = std::path::Path::new(&path);
+        grape::process_file(&args, path, &mut buf)?;
+        pb.inc(1);
+    }
+
+    pb.finish_with_message("Done!\n\n");
+
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    handle.write_all(buf.as_bytes())?;
+    handle.flush()?;
+
+    if args.stats {
+        println!(
+            "\nAnalyzed {} files in {} directories.",
+            file_count, directory_count
+        );
+    }
+
+    Ok(())
+}
